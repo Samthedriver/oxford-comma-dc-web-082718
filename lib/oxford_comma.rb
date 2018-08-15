cf8b4edf7713e1198ab78af9db17d9aaef35c204
github username <github email address>
2018-08-15 18:50:51 -0400

    Developed method to convert array to string.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
 def oxford_comma(array)
+  if array == []
+    return ""
+  end
+
+  index = 0
+  newstring = ""
+  while index < array.length do
+    if index == 0
+      newstring << array[index]
+    else
+      if index + 1 == array.length
+        if array.length == 2
+          newstring << " and " + array[index]
+        else
+          newstring << ", and " + array[index]
+        end 
+      else
+        newstring << ", " + array[index]
+      end
+    end
+    index += 1
+  end
+
+  newstring
 
 end
